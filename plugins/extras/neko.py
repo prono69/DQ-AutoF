ee586f04b46eeca7660ba6c4ff3f7b94a98270f6
faltu9851@gmail.com <faltu9851@gmail.com>
2023-12-01 21:40:18 +0530

    update neko.py

--- plugins/extras/neko.py
+++ plugins/extras/neko.py
@@ -1,13 +1,13 @@
 import os
 import random
 import requests
-from plugins.helpers.nsfw_var import nsfw as useless
+from plugins.helpers import nsfw_var as useless
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 
 @Client.on_message(filters.command("ne"))
 async def neko(client, message):
     "Search images from nekos"
-    reply_to = message.reply_to_message
     reply = message.reply_to_message
     reply_id = reply.message_id if reply else None
     choose = message.text.split(" ", 1)[1]
@@ -15,7 +15,6 @@ async def neko(client, message):
     if choose not in useless.nekos():
         return await catevent.edit(f"**Wrong catagory!! Choose from here:**\n\n{useless.nsfw(useless.nekos())}")
     link = useless.nekos(choose)
-    caption = choose
     await catevent.delete()
     try:
         if link.endswith(".gif"):
@@ -24,3 +23,65 @@ async def neko(client, message):
         	await client.send_photo(chat_id=message.chat.id, photo=link, caption=choose, reply_to_message_id=reply_id)
     except Exception as e:
         await message.reply_text(e)
+        
+        
+        
+        
+# This list is for waifu.im
+ISFW = [
+    "maid",
+    "marin-kitagawa",
+    "mori-calliope",
+    "oppai",
+    "raiden-shogun",
+    "selfies",
+    "uniform",
+    "waifu",
+]
+
+INSFW = [
+    "ass",
+    "ecchi",
+    "ero",
+    "hentai",
+    "milf",
+    "oral",
+    "paizuri",
+]
+
+waifu_help = "**🔞 NSFW** :  "
+for i in INSFW:
+    waifu_help += f"`{i.lower()}`   "
+waifu_help += "\n\n**😇 SFW** :  "
+for m in ISFW:
+    waifu_help += f"`{m.lower()}`   "
+
+
+@Client.on_message(filters.command("wi"))
+async def _(client, message):
+    "Search images from waifu.im"
+    reply = message.reply_to_message
+    reply_id = reply.message_id if reply else None
+    choose = message.text.split(" ", 1)[1]
+    url = "https://api.waifu.im"
+    if choose == "":
+        url = "{url}/search/"
+    elif choose in ISFW:
+        url = f"{url}/search/?included_tags={choose}&is_nsfw=null"
+    else:
+        url = f"{url}/search/?included_tags={choose}"
+    if choose not in waifu_help:
+        return await message.reply_text(f"**Wrong Category!! Choose from below**\n\n{waifu_help}")
+    catevent = await message.reply_text("`Processing...`")
+    resp = requests.get(url).json()
+    link = resp["images"][0]["url"]
+    # source = resp["images"][0]["source"]
+    btn = IKM([[IKB("💦 Source", url=f"{resp["images"][0]["source"]}")]])
+    try:
+        if link.endswith(".gif"):
+        	await client.send_animation(chat_id=message.chat.id, animation=link, caption=choose, reply_markup=btn, reply_to_message_id=reply_id)
+        else:
+        	await client.send_photo(chat_id=message.chat.id, photo=link, caption=choose, reply_markup=btn, reply_to_message_id=reply_id)
+    except Exception as e:
+        await message.reply_text(e)
+    await catevent.delete()    
